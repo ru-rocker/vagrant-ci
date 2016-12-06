5b7adb897a2d5c0149a73d47cae525eda3d209ba
Ricky Martaputra <ricky.martaputra@allianz.co.id>
2016-12-06 13:20:55 +0700

    change space

--- sample-code/src/main/java/com/rurocker/example/DemoApplication.java
+++ sample-code/src/main/java/com/rurocker/example/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
